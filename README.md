c84076800d80cb29483762d0758ce639940f6dc3
EmorZz1G <1227117695@qq.com>
2025-11-07 16:31:28 +0800

    upd

--- README.md
+++
@@ -1,4 +0,0 @@
-# STAND
-
-# Installation (Python==3.12.7)
-> pip install -r requirements.txt
--- src/models/supervised/__init__.py
+++
@@ -1,3 +0,0 @@
-from .STAND import STAND
-from .core_ml_wrap import KNN, LR, RF, SVM, AdaBoost, ExtraTrees, LightGBM
-
