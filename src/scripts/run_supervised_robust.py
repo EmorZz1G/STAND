cf668576d20e82ab782049e902955988377c0070
EmorZz1G <csemor@mail.scut.edu.cn>
2026-03-13 16:01:49 +0800

    fix

--- src/scripts/run_supervised_robust.py
+++ src/scripts/run_supervised_robust.py
@@ -101,22 +101,51 @@ if __name__ == '__main__':
     # train_test_split_list = [0.3, 0.4, 0.5]
     # train_test_split_list = [0.5]
     noise_prob_list = [0.01, 0.03, 0.05, 0.07, 0.1]
-    noise_prob_list = [0.05, 0.07, 0.1]
-    noise_prob_list = [0.01, 0.03, 0.05]
+    # noise_prob_list = [0.05, 0.07, 0.1]
+    # noise_prob_list = [0.01, 0.03, 0.05]
     # noise_prob_list = [0.01, 0.03, 0.05, 0.07, 0.1]
-
+    from itertools import product
+    task_list_generator = product(model_list, dataset_list, noise_prob_list)
+    task_list_generator = list(task_list_generator)
     #  nohup python src/scripts/run_supervised_robust.py 2>&1 | tee logs/log_file/robust2.txt &
     run_task = 0
     train_test_split = 0.5
-    for noise_prob in noise_prob_list:
-        if run_task == 0:
-            run_baselines(model_list, dataset_list, extra_config, train_test_split, noise_prob=noise_prob)
-            run_stand(dataset_list, train_test_split, noise_prob=noise_prob)
-        else:
-            # pass
-            for task in task_list:
-                model_list_ = task['model_list']
-                dataset_list_ = task['dataset_list']
-                extra_config_ = task.get('extra_config', extra_config)
-                run_baselines(model_list_, dataset_list_, extra_config_, train_test_split, noise_prob=noise_prob)
-                run_stand(dataset_list_, train_test_split, noise_prob=noise_prob)
+    parallel_jobs = 10
+
+    if parallel_jobs != 0:
+        import concurrent.futures
+        with concurrent.futures.ThreadPoolExecutor(max_workers=parallel_jobs) as executor:
+            futures = []
+            for model, dataset, noise_prob in task_list_generator:
+                model = [model]
+                dataset = [dataset]
+                if check_exist(model[0], dataset[0], noise_prob):
+                    print(f'Experiment for model: {model[0]}, dataset: {dataset[0]} with noise_prob: {noise_prob} already exists. Skipping...')
+                    continue
+                if model[0] == 'STAND':
+                    futures.append(executor.submit(run_stand, dataset, train_test_split, noise_prob=noise_prob))
+                else:
+                    futures.append(executor.submit(run_baselines, model, dataset, extra_config, train_test_split, noise_prob=noise_prob))
+            total = len(futures)
+            completed = 0
+            for future in concurrent.futures.as_completed(futures):
+                completed += 1
+                print(f"\033[34m已完成任务数: {completed}/{total}\033[0m")
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"Error occurred during execution: {e}")
+            
+    else:
+        for noise_prob in noise_prob_list:
+            if run_task == 0:
+                run_baselines(model_list, dataset_list, extra_config, train_test_split, noise_prob=noise_prob)
+                run_stand(dataset_list, train_test_split, noise_prob=noise_prob)
+            else:
+                # pass
+                for task in task_list:
+                    model_list_ = task['model_list']
+                    dataset_list_ = task['dataset_list']
+                    extra_config_ = task.get('extra_config', extra_config)
+                    run_baselines(model_list_, dataset_list_, extra_config_, train_test_split, noise_prob=noise_prob)
+                    run_stand(dataset_list_, train_test_split, noise_prob=noise_prob)
